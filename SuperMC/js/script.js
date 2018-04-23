b74b0f9bed6c41c41e46d920ffe02d33bc700479
Filipe Daineze <jimmyfilips@gmail.com>
2018-04-23 18:02:31 -0300

    atualização de compras

--- SuperMC/js/script.js
+++ SuperMC/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 	$('a[href^="#"]').on('click', function (e) {
 		e.preventDefault();
         /* Fecha o nav mobile ao clique */
-        if($('.navbar-toggle').css('display') !='none'){
+        if ($('.navbar-toggle').css('display') != 'none'){
             $('.navbar-toggle').trigger( "click" );
         }
         
@@ -17,5 +17,33 @@ $(document).ready(function () {
 			scrollTop: targetOffset
 		}, 500);
 	});
+    
+    /*  Mostral / Ocultar opções de compras
+    --------------------------------------------- */
+    $("a[rel=compra]").click( function(ev){
+        ev.preventDefault();
+ 
+        var id = $(this).attr("data-compra");
+     
+        //colocando o fundo preto
+        $('.mascara').show();
+ 
+        //posicionando modal
+        var left = 0 //($(window).width() /2) - ( $(id).width() / 2 );
+        var top = ($(window).height() / 2) - ( $(id).height() / 2 );
+     
+        $(id).css({'top':top,'left':left});
+        $(id).show();   
+    });
+ 
+    $(".mascara").click( function(){
+        $(this).hide();
+        $(".modal_compra").hide();
+    });
+ 
+    $('.prod-close').click( function(){
+        $(".mascara").hide();
+        $(".modal_compra").hide();
+    });
     
 });
