1100b9ff4c9ad4bec8e4031387af4d00eff90db4
Filipe Daineze <jimmyfilips@gmail.com>
2018-06-03 00:32:13 -0300

    tags + correção menu collapse

--- js/script.js
+++ js/script.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
     $('.page-load').addClass('effect');
     $('.page-loaded').removeClass('effect');
     var atual = '#home';
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
+    
+    $('.navbar-collapse a[href^="#"]').on('click', function (e) {
         setTimeout(function () {
             if ($('.navbar-toggle').css('display') != 'none') $('.navbar-toggle').trigger("click");
         }, 1000);
+    });
+    
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
         var id = $(this).attr('href');
         if (id == "#") id = "#home";
         $('.full-size'+atual).addClass('achatado');
