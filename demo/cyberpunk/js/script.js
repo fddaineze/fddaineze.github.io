4113f7c3adcb3e76747e58735b30d71843f97398
Filipe Daineze <jimmyfilips@gmail.com>
2018-11-01 15:41:36 -0300

    sync

--- demo/cyberpunk/js/script.js
+++ demo/cyberpunk/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     // ------------------------------------------
-    //  Funções de Menu
+    //  Funções de Menu -
     // ------------------------------------------
 
     $("#sidebar").mCustomScrollbar({
