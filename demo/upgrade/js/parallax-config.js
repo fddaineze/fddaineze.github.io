137187f07b29863e752415aa8aed96e926cb6136
Filipe Daineze <jimmyfilips@gmail.com>
2018-11-18 17:29:39 -0200

    effects upgrade

---
+++ demo/upgrade/js/parallax-config.js
@@ -0,0 +1,43 @@
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+
+function parallaxScroll(){
+/* * * * * *
+1 - ct
+2 - raven
+3 - udyr
+4 - tek
+* * * * * */
+    
+    const md = window.matchMedia( "(min-width: 768px)" );
+    const lg = window.matchMedia( "(min-width: 992px)" );
+    
+    if (lg.matches) {
+        var pbg1 = -10, p1 = 'bottom',
+            pbg2 = 100, p2 = 'top',
+            pbg3 = -265, p3 = 'bottom',
+            pbg4 = 500; p4 = 'top',
+        console.log('lg');
+    } else {
+        if (md.matches) {
+            var pbg1 = -400, p1 = 'bottom',
+                pbg2 = 300, p2 = 'bottom',
+                pbg3 = -390, p3 = 'bottom',
+                pbg4 = 5000, p4 = 'top';
+            console.log('md');
+        } else {
+            var pbg1 = -100, p1 = 'top',
+                pbg2 = 200, p2 = 'top',
+                pbg3 = -530, p3 = 'bottom',
+                pbg4 = 5000; p4 = 'top',
+            console.log('sm');
+        }
+    }
+    
+    var scrolled = $(window).scrollTop();
+    $('#parallax-bg1').css(p1,(pbg1+(scrolled*.32))+'px');
+    $('#parallax-bg2').css(p2,(pbg2-(scrolled*.15))+'px');
+    $('#parallax-bg3').css(p3,(pbg3+(scrolled*.15))+'px');
+    $('#parallax-bg4').css(p4,(pbg4-(scrolled*.33))+'px');
+}
