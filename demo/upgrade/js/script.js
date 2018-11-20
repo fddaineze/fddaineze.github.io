61f4e48924a9e9c542ccf8889e37728f6cdd7835
Filipe Daineze <jimmyfilips@gmail.com>
2018-11-20 14:03:30 -0200

    scroll correction

--- demo/upgrade/js/script.js
+++ demo/upgrade/js/script.js
@@ -6,13 +6,19 @@ $.extend($.easing,
 });
 // fim do plugin
 
-
 $("a").click(function(e){
    e.preventDefault();
    
+   const md = window.matchMedia( "(min-width: 768px)" );
    var anc = this.hash;
+    
+   if (md.matches) {
+      var adjust = 66;
+   } else {
+      var adjust = 0;
+   }
    
    $("html, body").animate({
-      scrollTop: $(anc).offset().top-66
+       scrollTop: $(anc).offset().top-adjust
    }, { duration: 1000, easing: 'easeOutCirc'});
 });
